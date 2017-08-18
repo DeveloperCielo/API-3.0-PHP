41f13753071b07ac307c07f97826d3b66c35d5d5
Murillo Vasconcelos <murillo.vasconcelos@xpd.com.br>
2017-08-18 10:23:23 -0300

    Acrescenta interface para atualização de pagamento recorrente

---
+++ src/Cielo/API30/Ecommerce/Request/UpdateRecurrentPaymentRequest.php
@@ -0,0 +1,54 @@
+<?php
+namespace Cielo\API30\Ecommerce\Request;
+
+use Cielo\API30\Ecommerce\Request\AbstractRequest;
+use Cielo\API30\Environment;
+use Cielo\API30\Merchant;
+use Cielo\API30\Ecommerce\RecurrentPayment;
+
+class UpdateRecurrentPaymentRequest extends AbstractRequest
+{
+
+    private $environment;
+
+    private $type;
+
+    private $param;
+
+    public function __construct($type, Merchant $merchant, Environment $environment)
+    {
+        parent::__construct($merchant);
+
+        $this->environment = $environment;
+
+        $this->type = $type;
+
+        $this->param = null;
+    }
+
+    public function execute($recurrentPaymentId)
+    {
+        $url = $this->environment->getApiQueryURL() . '1/RecurrentPayment/' . $recurrentPaymentId . '/' . $this->type;
+
+        return $this->sendRequest('PUT', $url, $this->param);
+    }
+
+    /**
+     * @return null
+     */
+    public function getParam() {
+        return $this->param;
+    }
+
+    /**
+     * @param null $param
+     */
+    public function setParam( $param ) {
+        $this->param = $param;
+    }
+
+    protected function unserialize($json)
+    {
+        return RecurrentPayment::fromJson($json);
+    }
+}
